deb4c0fe50d47ac3162b5f10397722cd02cda773
Nishii <nishimehra107@gmail.com>
2026-07-02 16:49:19 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+rows=int(input("enter the number of rows:"))
+columns=int(input("enter the number of columns:"))
+symbol=input("enter a symbol to use:")
+ 
+for x in range(rows):
+    for y in range(columns):
+      print(symbol , end=" ")
+    print()
